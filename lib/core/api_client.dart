6fffa7fb70d0611b138f9a168d059b99a58f2ea9
JN513 <jn5.guape@gmail.com>
2022-07-28 23:30:20 -0300

    Removendo coisas desnecessarias

--- lib/core/api_client.dart
+++ lib/core/api_client.dart
@@ -8,7 +8,7 @@ Future<double> getCurrentCotation(String from, String to) async{
   final response = await http.get(Uri.parse(url+"?f=$from&to=$to"));
 
   if (response.statusCode == 200) {
-    return double.parse(jsonDecode(response.body)["valor"]);
+    return jsonDecode(response.body)["valor"];
   } else {
     throw Exception('Falha ao buscar cotação');
   }
