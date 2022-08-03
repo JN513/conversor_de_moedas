6afb1d318b6e48b0514a5ea28b652488fcf663bf
JN513 <jn5.guape@gmail.com>
2022-08-02 21:12:14 -0300

    criando layout da pagina de juros compostos

--- lib/pages/compost.dart
+++ lib/pages/compost.dart
@@ -10,6 +10,68 @@ class CompostPage extends StatefulWidget {
 
 class _CompostPageState extends State<CompostPage> {
 
+  final TextEditingController _initialValueTextControler = TextEditingController();
+  final TextEditingController _monthlyValueTextControler = TextEditingController();
+  final TextEditingController _rateTextControler = TextEditingController();
+  final TextEditingController _periodValueTextControler = TextEditingController();
+  final TextEditingController _finalValueTextControler = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _initialValueTextControler.text = "0";
+      _monthlyValueTextControler.text = "0";
+      _rateTextControler.text = "0";
+      _periodValueTextControler.text = "0";
+      _finalValueTextControler.text = "0";
+    });
+  }
+
+  void calculate(){
+    String initialValueText = _initialValueTextControler.text;
+    String monthlyValueText = _monthlyValueTextControler.text;
+    String rateText = _rateTextControler.text;
+    String periodText = _periodValueTextControler.text;
+    String finalValueText = _finalValueTextControler.text;
+
+    double initialValue = 0.0;
+    double monthlyValue = 0.0;
+    double rate = 0.0;
+    double period = 0.0;
+    double finalValue = 0.0;
+
+    if(initialValueText.isEmpty) {
+      initialValue = 0.0;
+    } else {
+      initialValue = double.parse(initialValueText);
+    }
+
+    if(monthlyValueText.isEmpty) {
+      monthlyValue = 0.0;
+    } else {
+      monthlyValue = double.parse(monthlyValueText);
+    }
+
+    if(rateText.isEmpty) {
+      rate = 0.0;
+    } else {
+      rate = double.parse(rateText);
+    }
+
+    if(periodText.isEmpty) {
+      period = 0.0;
+    } else {
+      period = double.parse(periodText);
+    }
+
+    if(finalValueText.isEmpty) {
+      finalValue = 0.0;
+    } else {
+      finalValue = double.parse(finalValueText);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +83,90 @@ class _CompostPageState extends State<CompostPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("Juros Compostos"),
+            Padding(
+              padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+              child: Text("Valor inicial:"),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+              child: TextField(
+                controller: _initialValueTextControler,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Valor inicial:",
+                ),
+              ),
+            ),Padding(
+              padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+              child: Text("Valor Mensal:"),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+              child: TextField(
+                controller: _monthlyValueTextControler,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Valor Mensal:",
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+              child: Text("Taxa de juros:"),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+              child: TextField(
+                controller: _rateTextControler,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Taxa de juros:",
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+              child: Text("Periodo:"),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+              child: TextField(
+                controller: _periodValueTextControler,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Periodo:",
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+              child: Text("Valor final:"),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+              child: TextField(
+                controller: _finalValueTextControler,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Valor final",
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+              child: ElevatedButton(
+                child: Text("Calcular"),
+                onPressed: () {
+                  calculate();
+                },
+              ),
+            ),
           ],
         ),
       ),
